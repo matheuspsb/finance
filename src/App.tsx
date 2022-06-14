ddd394a481f6da1899fbdc6a4715bebf64511ae5
Matheus <matheusslg1@gmail.com>
2022-06-14 12:54:52 -0300

    feat: dateFilter

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { categories } from './data/categories';
 import { items } from './data/items';
 import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
 import { TableArea } from './components/TableArea';
+import { InfoArea } from './components/InfoArea'
 
 const App = () => {
 
@@ -16,6 +17,10 @@ const App = () => {
   useEffect(()=>{
     setFilteredList( filterListByMonth(list, currentMonth) );
   }, [list, currentMonth]);
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  }
   
   return(
     <C.Container>
@@ -23,7 +28,14 @@ const App = () => {
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
+        
+        <InfoArea 
+          currentMonth={currentMonth} 
+          onMonthChange={handleMonthChange}
+        />
+        
         <TableArea list={filteredList}/>
+
       </C.Body>
     </C.Container>
   );
