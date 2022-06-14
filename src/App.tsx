adca67d2714a32e70bd83cb07e19a079ada76d55
Matheus <matheusslg1@gmail.com>
2022-06-14 12:23:24 -0300

    feat: MUITA COISA ESQUECI DE COMITAR

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { Item } from './types/Item'
 import { Category } from './types/Category'
 import { categories } from './data/categories';
 import { items } from './data/items';
-import { getCurrentMonth } from './helpers/dateFilter';
+import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter';
+import { TableArea } from './components/TableArea';
 
 const App = () => {
 
@@ -13,7 +14,7 @@ const App = () => {
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
   
   useEffect(()=>{
-    
+    setFilteredList( filterListByMonth(list, currentMonth) );
   }, [list, currentMonth]);
   
   return(
@@ -22,9 +23,7 @@ const App = () => {
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        
-
-
+        <TableArea list={filteredList}/>
       </C.Body>
     </C.Container>
   );
