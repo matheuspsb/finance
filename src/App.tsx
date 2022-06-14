2a4ad24cce5bd71ec4e6458b4cb0963005368060
Matheus <matheusslg1@gmail.com>
2022-06-14 11:29:07 -0300

    feat: new folder created /helpers /components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { useState, useEffect } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item'
+import { Category } from './types/Category'
+import { categories } from './data/categories';
+import { items } from './data/items';
+import { getCurrentMonth } from './helpers/dateFilter';
 
 const App = () => {
+
+  const [list, setList] = useState(items);                                   /* Lista Completa */
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  
+  useEffect(()=>{
+    
+  }, [list, currentMonth]);
+  
   return(
     <C.Container>
       <C.Header>
@@ -8,7 +23,7 @@ const App = () => {
       </C.Header>
       <C.Body>
         
-        
+
 
       </C.Body>
     </C.Container>
---
+++ src/components/TableArea/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Table = styled.table`
+
+`;
+
+export const TableHeadColumn = styled.th`
+
+`;
