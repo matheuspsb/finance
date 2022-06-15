4245ad9459e3986ad29837aa9fa91cb983c0d14c
Matheus <matheusslg1@gmail.com>
2022-06-15 12:15:58 -0300

    feat: adding new caregories

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -3,5 +3,11 @@ import { Category } from '../types/Category'
 export const categories: Category = {
     food: { title: 'Alimentação', color: 'blue', expense: true},
     rent: { title: 'Aluguel', color: 'brown', expense: true},
-    salary: { title: 'Salário', color: 'green', expense: false}
+    transport: { title: 'Transporte', color: 'black', expense: true},
+    clothes: { title: 'Roupas', color: 'pink', expense: true},
+    others: {title: 'Outros', color: 'purple', expense: true},
+    salary: { title: 'Salário', color: 'green', expense: false},
+    pension: { title: 'Pensão', color: 'chartreuse', expense: false},
+    investments: { title: 'Investimentos', color: 'darkgreen', expense: false}
 }
+
