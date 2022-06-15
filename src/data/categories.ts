afb5ab3b158eb8de8f523179220c1d045de46a1d
Matheus <matheusslg1@gmail.com>
2022-06-15 12:52:55 -0300

    feat: new categories

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -5,9 +5,9 @@ export const categories: Category = {
     rent: { title: 'Aluguel', color: 'brown', expense: true},
     transport: { title: 'Transporte', color: 'black', expense: true},
     clothes: { title: 'Roupas', color: 'pink', expense: true},
+    signature: {title: 'Assinaturas', color: 'aqua', expense: true},
     others: {title: 'Outros', color: 'purple', expense: true},
     salary: { title: 'Salário', color: 'green', expense: false},
     pension: { title: 'Pensão', color: 'chartreuse', expense: false},
     investments: { title: 'Investimentos', color: 'darkgreen', expense: false}
 }
-
